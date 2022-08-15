ffdd38462ccf8c97c1deb676b2fcdbd4a04147c3
Carl <94771147+BeetJuicer@users.noreply.github.com>
2022-08-15 13:54:20 +0800

    trails

--- Assets/Scripts/Cat/Ghost.cs
+++ Assets/Scripts/Cat/Ghost.cs
@@ -6,12 +6,15 @@ public class Ghost : MonoBehaviour
 {
     private PlayerInputHandler inputHandler;
     [SerializeField]
-    private Cat enemy;
+    private Cat cat;
+    [SerializeField]
+    private Transform targetLocation;
 
     // Start is called before the first frame update
     void Start()
     {
         inputHandler = GetComponent<PlayerInputHandler>();
+        targetLocation.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -19,7 +22,17 @@ public class Ghost : MonoBehaviour
     {
         if (inputHandler.GetWhisperPressed())
         {
-            enemy.Call(transform);
+            targetLocation.gameObject.SetActive(false);
+            targetLocation.position = transform.position;
+            targetLocation.gameObject.SetActive(true);
+            cat.Call(targetLocation);
+        }
+
+        DebugPanel.Log("distance of target to cat", cat.gameObject.transform.position.x - targetLocation.position.x);
+
+        if (Mathf.Abs(cat.gameObject.transform.position.x - targetLocation.position.x) < 0.5f)
+        {
+            targetLocation.gameObject.SetActive(false);
         }
     }
 }
